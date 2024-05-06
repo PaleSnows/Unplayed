1f916b8a94c95f43287ea7b57442a5f01a662c41
Renzaki <renzakithakuri@gmail.com>
2024-05-06 08:02:21 +0545

    creating middleware to handle error

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
-import express, { json } from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-dotenv.config()
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
 
-const app =express()
-app.use(json())
-app.use(cors())
+const app = express();
+app.use(json());
+app.use(cors());
 
-app.listen(process.env.PORT,()=>{console.log(`Server running on port ${process.env.PORT}`);})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
